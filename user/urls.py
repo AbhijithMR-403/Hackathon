45b88cf0b4f528c7e73f07776dd03b08d3e15598
MARIYA <donjorois@gmail.com>
2024-04-06 22:20:56 +0530

    qiz template

--- user/urls.py
+++ user/urls.py
@@ -5,8 +5,10 @@ from user import views
 
 urlpatterns = [
 
-        path('login/',views.login,name='login'),
-        path('signup/',views.signup,name="signup")
+    path('login/',views.login,name='login'),
+    path('signup/',views.signup,name="signup"),
+    path('quiz/',views.quiz,name='quiz')
+
 
 ]
 
