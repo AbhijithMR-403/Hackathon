ffde92daf5deb49d4b90540d9e1252ba6251483f
Abhijith M R <abhijithmr581@gmail.com>
2024-04-06 22:24:18 +0530

    Login completed

--- user/urls.py
+++ user/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from user import views
 
 
-
 urlpatterns = [
 
-        path('login/',views.login,name='login'),
-        path('signup/',views.signup,name="signup")
+    path('login/', views.user_login, name='user_login'),
+    path('signup/', views.signup, name="user_signup")
 
 ]
-
-
